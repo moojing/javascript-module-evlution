f8e3dacd9b2c308aa6f1e33d6ec9021fce598697
moojing <starts789@gmail.com>
2021-10-19 00:29:33 +0800

    feat: add requireJS as the  AMD loader.

---
+++ module/index.js
@@ -0,0 +1,7 @@
+
+require(['./user','jquery'], function (user,jquery){
+
+    var user1 = user.getUser('Mujing', '1995/01/04') 
+    console.log('jquery :', jquery);
+    // other codes here 
+});
---
+++ module/user.js
@@ -0,0 +1,21 @@
+
+
+
+define(  function () {
+
+    var getUser = (name,birthday) => (function (){
+        var age = Math.floor((Date.now() - new Date(birthday).getTime())/1000/60/60/24/365);
+        return {
+            name,
+            age: Math.floor(age),
+            greeting: function greeting () {
+                console.log(`Hello, it's ${name} here. I'm ${age} years old.`) 
+            }
+        }
+    
+    })()
+    
+    return {
+        getUser
+    }
+});
